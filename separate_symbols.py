bc8cfbd24b7a933e4108499a5cb6a17f9d82549a
dawsonxiong <dawsonxiong@gmail.com>
2024-11-06 03:11:19 -0500

    Update separate_symbols.py
    creating contour outlines

--- separate_symbols.py
+++ separate_symbols.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+############################# loading + pre-processing image #############################
 # loading image
 filename = 'D:\Code\latex.ly/test_2.jpg'
 
@@ -8,9 +9,18 @@ filename = 'D:\Code\latex.ly/test_2.jpg'
 img = cv2.imread(filename, 0) # grayscale
 img = cv2.GaussianBlur(img, (3, 3), 0) # smooth edges using gaussian blur
 img = cv2.adaptiveThreshold( # binary image using adaptive threshold
-  img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 75, 10)
+    img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 75, 10)
 img = cv2.bitwise_not(img) # inverting colours to white text on black bg
 
 # display image temporarily
 cv2.imshow("pre-processed image", img)
 cv2.waitKey(0)
+
+################################## detecting contours ####################################
+contours, hierarchy = cv2.findContours( # finding contours of image (of white shapes in black bg)
+    img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+# reduce number of points, approximating contours to polygons
+contours_poly = []
+for c in contours:
+    contours_poly.append(cv2.approxPolyDP(c, 3, True))
