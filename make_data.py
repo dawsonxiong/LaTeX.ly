a1d38e7fb643c794fc394799e3bd5cf30560e0a8
Dawson Yi Xiong <dawsonxiong@gmail.com>
2025-01-19 23:25:32 -0500

    Update make_data.py

--- make_data.py
+++ make_data.py
@@ -4,9 +4,10 @@ import os
 
 math_symbols = [
     r'0', r'1', r'2', r'3', r'4', r'5', r'6', r'7', r'8', r'9', r'+', r'-', r'=', r',',
-    r'a', r'b', r'c', r'd', r'e', r'f', r'g', r'h', r'i', r'j', r'k', r'l', r'm', r'n', 
-    r'o', r'p', r'q', r'r', r's', r't', r'u', r'v', r'w', r'x', r'y', r'z', r'\{', r'\}', 
-    r'(', r')', r'!', r'|', r'<', r'>', r'/',
+    r'a', r'b', r'c', r'd', r'e', r'f', r'g', r'h', r'i', r'j', r'k', r'm', r'n', 
+    r'o', r'p', r'q', r'r', r's', r't', r'u', r'v', r'w', r'x', r'y', r'z', 
+    r'A', r'B', r'C', r'E', r'F', r'G', r'I', r'N', r'P', r'R', r'S', r'T', r'X',
+    r'\{', r'\}', r'(', r')', r'!', r'|', r'<', r'>', r'/',
     r'\supset', r'\supseteq', r'\nsupset', r'\subset', r'\subseteq', r'\nsubset', r'\setminus',
     r'\cap', r'\cup', r'\exists', r'\in', r'\notin',  r'\forall', r'\emptyset', 
     r'\propto',  r'\rightarrow', r'\leftarrow', r'\Rightarrow', r'\Leftarrow', r'\perp', r'\parallel',
@@ -18,9 +19,10 @@ math_symbols = [
 ]
 symbols = [
     r'0', r'1', r'2', r'3', r'4', r'5', r'6', r'7', r'8', r'9', r'+', r'-', r'=', r',',
-    r'a', r'b', r'c', r'd', r'e', r'f', r'g', r'h', r'i', r'j', r'k', r'l', r'm', r'n', 
-    r'o', r'p', r'q', r'r', r's', r't', r'u', r'v', r'w', r'x', r'y', r'z', r'\{', r'\}', 
-    r'(', r')', r'!', r'|', r'<', r'>', r'/'
+    r'a', r'b', r'c', r'd', r'e', r'f', r'g', r'h', r'i', r'j', r'k', r'm', r'n', 
+    r'o', r'p', r'q', r'r', r's', r't', r'u', r'v', r'w', r'x', r'y', r'z', 
+    r'A', r'B', r'C', r'E', r'F', r'G', r'I', r'N', r'P', r'R', r'S', r'T', r'X',
+    r'\{', r'\}', r'(', r')', r'!', r'|', r'<', r'>', r'/'
 ]
 
 math_fonts = ["cm", "stix", "stixsans", "dejavuserif", "dejavusans"]
